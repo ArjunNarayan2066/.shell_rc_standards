b5b4aa2dbdd4061a81758937aaeecfa42d085f7b
Arjun Narayan <arnarayan@tesla.com>
2020-02-03 14:58:36 -0800

    Add time saved script

--- manifest.sh
+++ manifest.sh
@@ -4,4 +4,4 @@ source ~/.shell_rc_standards/useful_shortcuts.sh
 
 export EDITOR='vim'
 alias update="sudo apt-get update && sudo apt-get upgrade"
-
+alias get_time_saved="python3 ~/.shell_rc_standards/time_saved.py"
---
+++ time_saved.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python3
+
+from pathlib import Path
+
+target = Path(__file__).parent.joinpath("../time_saved_using_alias_commands.txt").resolve()
+
+total_time = 0
+
+with open(target, 'r+') as f:
+    for line in f:
+        if line:
+            total_time += int(line.split(" ")[0])
+
+print("Total Time: {}s".format(total_time))
