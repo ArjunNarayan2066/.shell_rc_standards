7992782bea344a0cb73f71380a40c3fca6041df7
Arjun Narayan <arjunn@rogers.com>
2020-02-04 03:25:29 +0000

    Tested and fixed issues

---
+++ continue_setup.zsh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+echo "In zsh setup"
+
+echo "source ~/.shell_rc_standards/manifest.sh" >> ~/.zshrc
+
+echo "DONE"
+
--- setup_linux.sh
+++ setup_linux.sh
@@ -5,13 +5,14 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-# Install ZSH
-sudo apt-get install zsh
+if [ ! -d "~/.oh-my-zsh" ]
+then
+	sudo apt-get install zsh
 
-# Install oh-my-zsh https://github.com/ohmyzsh/ohmyzsh
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	# Install oh-my-zsh https://github.com/ohmyzsh/ohmyzsh
+	sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Set zsh to default shell
-chsh -s $(which zsh)
+	# Set zsh to default shell
+	chsh -s $(which zsh)
+fi
 
-echo "source $PWD/manifest.sh" >> ~/.zshrc
